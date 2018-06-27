e606d141e5a0a2a56c83379e201d3828c3cacce2
AryaMirshafii <amirshafii3@gatech.edu>
2018-06-27 11:54:23 -0700

    Final Version Uploaded
    The main code for this project is located in nileReverbfinalVersion.ino
    Added checks to ensure that the device waits for a command to finish before executing another.

--- hotelCaliforniaTest/hotelCaliforniaTest.ino
+++ hotelCaliforniaTest/hotelCaliforniaTest.ino
@@ -1,36 +1,64 @@
 #include "SoftwareSerial.h"
+#include <LiquidCrystal.h>
 //Continuation of bluetoothTest, but it will play 
 //Hotel California on my phone when a button is pressed on the circuit.
 SoftwareSerial mySerial(10,11); //RX, TX
-const int buttonPin = 2;
+const int buttonPin = 9;
 int buttonHighLow = 0; 
 String currentItem;
-String command = "_play sharp dressed woman _";    //Testing LCS algorithm in app. Supposed to play Sharp Dressed MAN by ZZ top.
+//String command = "_play sharp dressed woman_";    //Testing LCS algorithm in app. Supposed to play Sharp Dressed MAN by ZZ top.
+String command = "_play some foreigner_";
+String serialRead = "";
+
+int buttonValue;
+
+LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
 void setup() {
   
-  
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
   
   Serial.begin(9600); //Initialize normal serial 
   mySerial.begin(9600); //Can be changed on AT command mode
   pinMode(buttonPin, INPUT);    // Initialize pin 2 as button pin
+  //lcd.print("hello, world!");
 }
 
 void loop() {
-  buttonHighLow = digitalRead(buttonPin);
-  mySerial.println("nothing");
-  delay(800);
-  if (buttonHighLow == HIGH) {
-    Serial.println(sizeof(command));
-    mySerial.println(command);
-    delay(800);
-  }else {
-    Serial.println("Not playing anything");
-  }
+//  buttonHighLow = digitalRead(buttonPin);
+//  
+//  mySerial.println("nothing");
+//  delay(800);
+//  if (buttonHighLow == HIGH) {
+//    Serial.println(sizeof(command));
+//    mySerial.println(command);
+//    delay(800);
+//  }else {
+//    Serial.println("Not playing anything");
+//  }
+//
+//
+//  if( mySerial.available()){
+//    Serial.println("Data being read is");
+//    serialRead += char(mySerial.read());
+//    
+//  }
+//  if(serialRead.length() > 0){
+//    Serial.println("The data read is " + serialRead);
+//  }
+//
+//
+//  buttonValue = analogRead(A2);
+//  Serial.println("Button Value is " + String(buttonValue));
+//
+//  
+//  //lcd.clear();
 
-  
-  
-  
-  
+  lcd.print("  CIRCUIT DIGEST");//print name
+
+  lcd.setCursor(0, 1); // set the cursor to column 0, line 2
+  delay(1000);
+  //lcd.clear();
   
 }
 
---
+++ nileReverbFinalVersion/nileReverbFinalVersion.ino
@@ -0,0 +1,384 @@
+#include <LiquidCrystal.h>
+#include "SoftwareSerial.h"
+#include "Timer.h"
+
+
+//Initialize a software serial
+SoftwareSerial mySerial(12, 13); //RX, TX
+
+
+//Ints
+int button1HighLow = 0;      // The value of button 1 either High or low
+int button2HighLow = 0;      // The value of button 2 either High or low
+int button3HighLow = 0;      // The value of button 3 either High or low
+int button4HighLow = 0;      // The value of button 4 either High or low
+int currentTemp = 0;
+
+
+
+//INitializing the LCD
+LiquidCrystal lcd(0, 1, 8, 9, 10, 11); //Initialize LCD
+
+
+//Button Pins
+const int button1Pin = 7;
+const int button2Pin = 6;
+const int button3Pin = 5;
+const int button4Pin = 4;
+
+//
+String currentItem;
+String command = "";    //The command to be excecuted by the system.
+
+String bluetoothInput = "";
+
+
+// Booleans
+boolean isPlayingMusic = false;
+boolean isAsking = false;
+
+
+
+
+
+//Choices that the user can be asked
+String choices[3];
+
+
+
+
+boolean isReading = false;
+// The main setup function
+void setup() {
+
+
+  lcd.begin(16, 2);
+  Serial.begin(57600); //Initialize normal serial
+  mySerial.begin(9600); //Can be changed on AT command mode
+
+
+  pinMode(button1Pin, INPUT);    // Initialize pin 7 as button pin
+  pinMode(button2Pin, INPUT);    // Initialize pin 6 as button pin
+  pinMode(button3Pin, INPUT);    // Initialize pin 5 as button pin
+  pinMode(button4Pin, INPUT);    // Initialize pin 4 as button pin
+
+}
+
+
+
+
+// The main loop function
+void loop() {
+
+  //mySerial.println("");
+  if (Serial.available() && !isReading) {
+
+    //This section of code capitalizes the letters after a space in a command
+    if (command[command.length() - 1] == ' ') {
+      char ardunioData = char(Serial.read());
+
+      Serial.println(command);
+      
+      //this part ensures that the character being capitalized is an actual character
+      int charAsInt = int(ardunioData);
+      if ((charAsInt > 64 & charAsInt < 91) || (charAsInt > 96 & charAsInt < 123)) {
+        ardunioData = int(ardunioData) - 32;
+      }
+
+      command += ardunioData;
+    } else {
+      command += char(Serial.read());
+    }
+
+
+  }
+
+
+  if (command != "" && command[command.length() - 1] == '\n') {
+    execute(command);
+  }
+
+
+
+  readButtons();
+
+
+  screen();               //The function that keeps the screen running
+  checkInputBTData();     //Checking for communication from phone to ardunio.
+
+}
+
+
+
+
+
+
+
+
+// A function that checks for questions from the bluetooth serial.
+Timer t;
+void checkInputBTData() {
+
+  if (mySerial.available()) {
+    isReading = true;
+    bluetoothInput +=  char(mySerial.read());
+    Serial.println(bluetoothInput);
+  }else {
+    isReading = false;
+    bluetoothInput = "";
+  }
+  
+  int lastIndex = bluetoothInput.length() - 1;
+  if ((bluetoothInput.length() > 3) && (bluetoothInput[0] == '_' && bluetoothInput[lastIndex] == '_')) {
+    Serial.println("Executing command" + bluetoothInput.length());
+    execute(bluetoothInput);
+    bluetoothInput = "";
+    isReading = false;
+
+  }
+
+
+
+}
+
+
+
+
+
+//Execute a command from the Nile Reverb
+void execute(String theCommand) {
+  if (theCommand[0] != '_') {
+
+    theCommand = '_' + theCommand;
+  }
+
+  if (!isReading) {
+    if (theCommand[theCommand.length() - 1] == '\n') {
+      theCommand[theCommand.length() - 1] =  '_';
+    }
+  }
+
+
+
+  Serial.println("The command is " + theCommand);
+  if (theCommand.indexOf("go Next") > 0 || theCommand.indexOf("go Forward") > 0) {
+    mySerial.println(theCommand);
+    Serial.println("Skipping forward");
+
+  } else if (theCommand.indexOf("go Previous") > 0 || theCommand.indexOf("go Back") > 0) {
+    mySerial.println(theCommand);
+    Serial.println("Skipping back");
+
+  } else if (theCommand.indexOf("Playing") > 0) {
+    Serial.println("Song playing has been started");
+    writeToScreen(theCommand, "");
+  } else if (theCommand.indexOf("play") > 0) {
+    mySerial.println(theCommand);
+    Serial.println("playing some music");
+  } else if (theCommand.indexOf("Ask") > 0 ) {
+    askQuestion(theCommand);
+
+  } else if (theCommand.indexOf("UpdateW") > 0 ) {
+    updateWeather(theCommand);
+  } else if (theCommand.indexOf("weather") > 0 ) {
+
+    Serial.println("Getting weather");
+    mySerial.println(theCommand);
+  }
+
+  //If the command still hasnt been executed at this point then it is probably invalid
+  command = "";
+  isReading = false;
+
+}
+
+
+
+
+
+//A function that updates the weather and writes it to the
+String city;
+int commaIndex;
+String tempString;
+String spaces;
+void updateWeather(String weatherString) {
+
+  weatherString.replace("_", "");
+  weatherString.replace("UpdateW", "");
+
+  commaIndex = weatherString.indexOf(',');
+  city = weatherString.substring(commaIndex + 1, weatherString.length());
+  Serial.println("The updated weather data is" + weatherString);
+  tempString = weatherString.substring(0, commaIndex);
+  currentTemp = tempString.toInt();
+
+
+  Serial.println("The temp is " + tempString);
+  Serial.println("The city is " + city);
+
+
+
+  if (tempString.length() % 2 == 0) {
+
+    tempString = tempString + " ";
+    // If the temperature is greater than 100 then add a space in front of it before printing
+    //Char 176 is the degree symbol
+    tempString = " Temp is: " + tempString + char(176) + "F ";
+  } else {
+
+    tempString = "", " Temp is: " + tempString + char(176) + "F ";
+  }
+
+  city = "In " + city;
+
+  writeToScreen(tempString, city);
+
+
+
+
+
+}
+
+// Function that asks a question and prints it to the screen
+void askQuestion(String question) {
+  question.replace("_", "");
+  Serial.println("The improved Question is" + question);
+  writeToScreen(question, "");
+
+
+  // Split string into choices using strtok
+
+  isAsking = true;
+
+}
+
+// A function that determines how buttons 2- 4 will be used
+// For example if the device is buttons will show back, pause, forward for songs
+// else if the device is asking the user a question, buttons will be used for choices
+int pauseButtonCount = 0;
+void readButtons() {
+
+
+  button1HighLow = digitalRead(button1Pin);
+
+
+  if (button1HighLow == HIGH) {
+    command = "_play wonderwall_";
+    Serial.println(sizeof(command));
+    mySerial.println(command);
+    writeToScreen("Playing Hotel California By: The Eagles", "");
+    Serial.println(sizeof("Playing Hotel California By: The Eagles"));
+    command = "";
+    delay(500);
+  } else {
+
+  }
+
+
+  if (isAsking) {
+
+
+    return;
+  }
+
+  if (isPlayingMusic) {
+    button2HighLow = digitalRead(button2Pin);
+    button3HighLow = digitalRead(button3Pin);
+    button4HighLow = digitalRead(button4Pin);
+
+
+
+
+
+    if (button2HighLow == HIGH) {
+      //Play previous song
+      command = "_Player goback_";
+      Serial.println("Going back");
+
+
+    } else if (button3HighLow = HIGH) {
+      //Pause or unpause the player
+      pauseButtonCount++;
+      if (pauseButtonCount % 2 == 1) {
+        command = "_Player pause_";
+        Serial.println("Pausing song");
+      } else if (pauseButtonCount % 2 == 0) {
+
+        Serial.println("Resuming song");
+        command = "_Player play_";
+
+      }
+
+    } else if (button4HighLow == HIGH) {
+      //Play next song
+      command = "_Player goforward";
+      Serial.println("Going forward");
+    }
+    mySerial.println(command);
+    delay(500);
+
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+//A function that is called in the main loop and is the code that makes the screen continue running
+void screen() {
+
+  lcd.setCursor(0, 1); // set the cursor to column 0, line 2
+  delay(700);//delay of 0.3sec
+
+  lcd.scrollDisplayRight();//shifting data on LCD
+
+  lcd.setCursor(0, 0);// set the cursor to column 0, line1
+}
+
+
+
+// A simple function that prints text to the lCD
+void writeToScreen(String line1, String line2) {
+  lcd.clear();
+
+  if (line2 != "" && line2.length() > 0) {
+    if (line2[0] == '_') {
+      line2[0] = ' ';
+    }
+
+    if (line2[line2.length() - 1] == '_') {
+      line2[line2.length() - 1] = ' ';
+    }
+    lcd.setCursor(0, 1); // set the cursor to column 0, line 2
+    lcd.print(line2);
+  }
+
+  if (line1 != "" && line1.length() > 0) {
+    if (line1[0] == '_') {
+      line1[0] = ' ';
+    }
+
+    if (line1[line1.length() - 1] == '_') {
+      line1[line1.length() - 1] = ' ';
+    }
+    lcd.setCursor(0, 0); // set the cursor to column 0, line 2
+    lcd.print(line1);
+  }
+
+
+
+
+
+
+}
+
+
+
